830905b9e20e84da029adbca7de7cbb0360c684f
github username <github email address>
2021-01-13 22:24:36 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
 
+def my_collect(collection)
+    i = 0
+    new_collection = []
+    while i < collection.length
+        new_collection.push(yield(collection[i]))
+        i += 1
+    end
+    new_collection
+end
+ 
+
